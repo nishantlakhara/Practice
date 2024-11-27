12f8213f1a1a1d0c757e5191c296108878aaab41
nishant lakhara <nishant.lakhara.280389@gmail.com>
2024-11-27 14:09:32 +0530

    java 8 design patterns

--- src/main/java/java8/designpatterns/PlayWithPredicates.java
+++ src/main/java/java8/designpatterns/PlayWithPredicates.java
@@ -1,4 +1,18 @@
 package java8.designpatterns;
 
+import java8.designpatterns.function.Predicate;
+
 public class PlayWithPredicates {
+    public static void main(String[] args) {
+        Predicate<String> p1 = s -> s != null;
+        Predicate<String> p2 = s -> !s.isEmpty();
+
+        Predicate<String> p3 = p1.and(p2);
+        System.out.println(p3.test(null));
+        System.out.println(p3.test(""));
+        System.out.println(p3.test("hello"));
+
+        Predicate<String> notP2 = p2.negate();
+        System.out.println(notP2.test("hello"));
+    }
 }
